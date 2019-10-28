050ea0ca9a4e55b43ffda2715c8333ff8a714994
Amy Pinedo <amairanip@gmail.com>
2019-10-27 17:28:47 -0700

    props and art

--- VGDAMurderMysteryGame/Assets/Scripts/SceneTransitionScript.cs
+++ VGDAMurderMysteryGame/Assets/Scripts/SceneTransitionScript.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
+using Fungus;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitionScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
+
+    private int levelToLoad;
+    
+    private BoxCollider2D bc2D;
+    
     void Start()
     {
+        bc2D = GetComponent<BoxCollider2D>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerMovement player = other.GetComponent<PlayerMovement>();
         
+        Scene CurrentScene = SceneManager.GetActiveScene();
+
+        if (player != null) ;
+        {
+            FadeToScene();
+        }
+    }
+    
+    public void FadeToScene (int levelIndex)
+    {
+        Scene CurrentScene = SceneManager.GetActiveScene();
+        
+        
+        
+        animator.SetTrigger("FadeOut");
+        
+        levelToLoad = levelIndex;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnFadeComplete()
     {
-        
+        SceneManager.LoadScene(levelToLoad);
     }
 }
