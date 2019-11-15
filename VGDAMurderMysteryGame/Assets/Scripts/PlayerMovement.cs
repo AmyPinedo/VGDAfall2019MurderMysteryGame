2916897698d239484db2a674fc12a7f72baa860a
Amy Pinedo <amairanip@gmail.com>
2019-11-15 14:33:21 -0800

    Audio and movement script edits

--- VGDAMurderMysteryGame/Assets/Scripts/PlayerMovement.cs
+++ VGDAMurderMysteryGame/Assets/Scripts/PlayerMovement.cs
@@ -8,33 +8,67 @@ public class PlayerMovement : MonoBehaviour
     public GameObject dialogueBox;
     public bool menuOpen = false;
     public bool inventoryOpen;
-    void Start()
+    private AudioSource playerAudioSource;
+    private bool playerMoving = false;
+    private Rigidbody2D rb2D;
+
+    private void Awake()
     {
-
+        playerAudioSource = GetComponent<AudioSource>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         ///Horizontal movement
         if (Input.GetKey(KeyCode.A) && menuOpen == false && dialogueBox.GetComponent<menuBehavior>().isOpen == false && inventoryOpen == false)
         {
             transform.position += new Vector3(-xMovement, 0.0f, 0.0f);
+            playerMoving = true;
         }
         else if (Input.GetKey(KeyCode.D) && menuOpen == false && dialogueBox.GetComponent<menuBehavior>().isOpen == false && inventoryOpen == false)
         {
             transform.position += new Vector3(xMovement, 0.0f, 0.0f);
+            playerMoving = true;
         }
 
         ///Vertical movement
-        if (Input.GetKey(KeyCode.W) && menuOpen == false && dialogueBox.GetComponent<menuBehavior>().isOpen == false && inventoryOpen == false)
+        else if (Input.GetKey(KeyCode.W) && menuOpen == false && dialogueBox.GetComponent<menuBehavior>().isOpen == false && inventoryOpen == false)
         {
             transform.position += new Vector3(0.0f, yMovement, 0.0f);
+            playerMoving = true;
         }
         else if (Input.GetKey(KeyCode.S) && menuOpen == false && dialogueBox.GetComponent<menuBehavior>().isOpen == false && inventoryOpen == false) 
         {
             transform.position += new Vector3(0.0f, -yMovement, 0.0f);
+            playerMoving = true;
+        }
 
+        else
+        {
+            playerMoving = false;
+        }
+        
+        HandlePlayerSound();
+    }
+
+    private void HandlePlayerSound()
+    {
+        if (playerMoving == true)
+        {
+            if (!playerAudioSource.isPlaying)
+            playerAudioSource.Play();
+            Debug.Log("playing sound");
+        }
+
+        else
+        {
+            if (playerAudioSource.isPlaying)
+            {
+                playerAudioSource.Stop();
+                Debug.Log("stopping sound");
+            }
         }
     }
 }
