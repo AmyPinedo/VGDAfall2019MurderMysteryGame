621565880e0c28f6ecf40d487a59ba9da9c00e8c
Amy Pinedo <amairanip@gmail.com>
2019-11-21 23:44:49 -0800

    pls kill me

--- VGDAMurderMysteryGame/Assets/Scripts/sceneChangerReg.cs
+++ VGDAMurderMysteryGame/Assets/Scripts/sceneChangerReg.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class sceneChangerReg : MonoBehaviour
 {
     public void playButtonScene()
     {
-        sceneChanger.changeScene("Final");
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void playCreditsScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void endGameButton()
+    {
+        Application.Quit();
     }
 }
